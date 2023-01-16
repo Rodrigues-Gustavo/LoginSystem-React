3ef46887779f373537cb41bca1dd46dfa19bbecb
Gustavo Rodrigues <Gustavorr001@gmail.com>
2023-01-16 11:58:48 -0300

    creating the app login and sign up page

--- src/pages/Signin/styles.js
+++ src/pages/Signin/styles.js
@@ -1,4 +1,3 @@
-import { findDOMNode } from "react-dom";
 import styled from "styled-components";
 
 export const Container = styled.div`
--- src/pages/Signup/index.jsx
+++ src/pages/Signup/index.jsx
@@ -1,9 +1,72 @@
-import React from 'react'
+import React from 'react';
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+import * as C from './styles';
+import { Link, useNavigate } from 'react-router-dom';
+import useAuth from '../../hooks/useAuth';
 
 const Signup = () => {
+  const [email, setEmail] = useState("");
+  const [emailConf, setEmailConf] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const navigate = useNavigate();
+
+  const { Signup } = useAuth();
+
+  const handleSignup = () => {
+    if (!email | !emailConf | !password) {
+      setError("Preencha todos os campos");
+      return;
+    } else if (password !== emailConf) {
+      setError("Os e-mails não são iguais");
+      return;
+    }
+
+    const res = Signup(email, password);
+    
+    if (res) {
+      setError(res);
+      return;
+    }
+
+    alert("Usuário cadastrado com sucesso!");
+    navigate("/");
+  }
+
   return (
-    <div>SignUp</div>
-  )
+    <C.Container>
+      <C.Label>SISTEMA DE LOGIN</C.Label>
+      <C.Content>
+      <Input 
+          type="email"
+          placeholder="Digite seu E-mail"
+          value={email}
+          onChange={(e) => [setEmail(e.target.value), setError("")]}
+        />
+        <Input 
+          type="email"
+          placeholder="Confirme seu E-mail"
+          value={emailConf}
+          onChange={(e) => [setEmailConf(e.target.value), setError("")]}
+        />
+        <Input 
+          type="password"
+          placeholder="Digite sua senha"
+          value={password}
+          onChange={(e) => [setPassword(e.target.value), setError("")]}
+        />
+        <C.LabelError>{error}</C.LabelError>
+        <Button text="Inscrever-se" onClick={handleSignup} />
+        <C.LabelSignin>
+          Já tem uma conta?
+          <C.Strong>
+            <Link to="/">&nbsp;Entre</Link>
+          </C.Strong>
+        </C.LabelSignin>
+      </C.Content>
+    </C.Container>
+  );
 }
 
 export default Signup
